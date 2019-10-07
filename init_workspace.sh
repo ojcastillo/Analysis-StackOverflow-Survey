90be98971bd29e439ec47a5e13618f1147d80680
Orlando Castillo <ojcm@twitch.tv>
2019-10-07 09:41:24 -0700

    Progress towards south america analysis

--- init_workspace.sh
+++ init_workspace.sh
@@ -1,10 +1,10 @@
 if ! conda env list | grep -q 'stackoverflow'; then
     echo "Didn't find a 'stackoverflow' conda env. Setting one for you."
-    conda create -n stackoverflow python=3.7
+    conda create -n stackoverflow python=3.7 pip
 fi
 
 echo "Activating stackoverflow env"
-source activate stackoverflow
+conda activate stackoverflow
 
 echo "Installing dependencies into stackoverflow env"
 pip install -r requirements.txt
